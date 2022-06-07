1bc77ea8d3f78f2c2f59a54a5d37c8c31366e6ff
Daniel <daniels.daenzer@gmail.com>
2022-06-07 20:25:42 +0200

    trying things

---
+++ bash_scripts/various_bash_commands.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd ../src || exit
+#python main_execute_method.py method=baseline model=vicreg trainset=cifar10 batch_size=4096
+python vicreg_evaluate.py --data-dir ../data --pretrained ../models/resnet50.pth --exp-dir ../outputs/VICReg_Training --lr-head 0.02
