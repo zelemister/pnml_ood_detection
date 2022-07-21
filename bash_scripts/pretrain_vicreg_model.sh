7dd6e7a2c9b5490cc2da4233095ed8c22c658687
Daniel <daniels.daenzer@gmail.com>
2022-07-21 12:43:06 +0200

    added pretrain svhn

--- bash_scripts/pretrain_vicreg_model.sh
+++ bash_scripts/pretrain_vicreg_model.sh
@@ -4,3 +4,5 @@ declare -a dataset = "cifar10"
 
 cd ../src || exit
 python vicreg_evaluate.py --data-dir ../data --pretrained ../models/resnet50.pth --exp-dir ../outputs/VICReg_Training --weights finetune --train-perc 10 --epochs 20 --lr-backbone 0.03 --lr-classifier 0.08 --weight-decay 0 --dataset dataset
+
+#python vicreg_evaluate.py --data-dir ../data --pretrained ../models/resnet50.pth --exp-dir ../outputs/VICReg_Training/CIFAR10 --weights finetune --train-percent 10 --epochs 20 --lr-backbone 0.03 --lr-head 0.08 --weight-decay 0 --dataset cifar10
--- bash_scripts/various_bash_commands.sh
+++ bash_scripts/various_bash_commands.sh
@@ -2,4 +2,4 @@
 
 cd ../src || exit
 #python main_execute_method.py method=baseline model=vicreg trainset=cifar10 batch_size=4096
-python vicreg_evaluate.py --data-dir ../data --pretrained ../models/resnet50.pth --exp-dir ../outputs/VICReg_Training --lr-head 0.02
+python vicreg_evaluate.py --data-dir ../data --pretrained ../models/resnet50.pth --exp-dir ../outputs/VICReg_Training --lr-head 0.02 --dataset svhn
